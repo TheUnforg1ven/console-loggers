77dd1857d7ebba2ad338ae4699282050e576160a
TheUnforg1ven <tbatjuk4u@gmail.com>
2020-12-23 18:43:30 +0200

    Added new localstorage code and loading function

--- js/channel.js
+++ js/channel.js
@@ -1,9 +1,20 @@
-export function getChannelId(){
-    let fetchedData = window.fetch(`https://wind-bow.glitch.me/helix/users?login=silvername`)
+export function getChannelId(channelName){
+    let fetchedData = window.fetch(`https://wind-bow.glitch.me/helix/users?login=${channelName}`)
                         .then(glitchResponse => glitchResponse.json());
     
+
     let channelId = Promise.resolve(fetchedData)
-                            .then(valueId => valueId['data']['0']['id']);
+                        .then(valueId => {
+                            if (valueId['data'] && valueId['data'].length !== 0) {
+                                return valueId['data']['0']['id'];
+                            }
+                            else {
+                                alert("No such streamer :C");
+                                return "No such streamer :C";
+                            }
+                        })
+                        .catch(error => console.log(error));
+    
 
     return channelId ?? "No such streamer :C";
 }
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,130 @@
 import * as Emotes from './emotes.js'
 import * as Channel  from './channel.js'
 
-const userEmotes = ['https://api.betterttv.net/3/cached/frankerfacez/users/twitch/70075625'];
-const channelEmotes = ['https://api.betterttv.net/3/cached/users/twitch/70075625'];
+let userEmotes = ['https://api.betterttv.net/3/cached/frankerfacez/users/twitch/70075625'];
+let channelEmotes = ['https://api.betterttv.net/3/cached/users/twitch/70075625'];
 
-Channel.getChannelId()
-        .then(value => console.log(value));
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+let tempList; 
+let tempId;
+
+let dataUE = localStorage.getItem("UElist");
+let dataCE = localStorage.getItem("CElist");
+
+// 'dataUE' change with 'userEmotes'
+if (dataUE.length == 1) {
+    tempList = [];
+    tempId = 0;
+    tempList.push({
+        id: tempId,
+        name: "SilverName",
+        channelId: 70075625,
+        trash: false
+    });
+    tempId++;
+}
+
+let dataT = localStorage.getItem("Tlist");
+
+// 'dataUE' change with 'userEmotes'
+if (dataUE.length == 1) {
+    localStorage.setItem("UElist",  JSON.stringify(userEmotes));
+    localStorage.setItem("CElist",  JSON.stringify(channelEmotes));
+    localStorage.setItem("Tlist",  JSON.stringify(tempList));
+}
+
+dataUE = localStorage.getItem("UElist");
+dataCE = localStorage.getItem("CElist");
+
+userEmotes = JSON.parse(dataUE);
+channelEmotes = JSON.parse(dataCE);
+
+if (dataT) {
+    tempList = JSON.parse(dataT);
+    tempId = tempList.length;
+    loadTlist(tempList);
+}
+
+function loadTlist(list) {
+    list.forEach(element => {
+        addChannelName(element.name, element.id, element.trash)
+    });
+}
+
+function addChannelName(channelName, id, trash) {
+    if (trash) return;
+
+    const item = `
+        <li class="item">
+            <p class="text">${channelName}</p>
+            <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+        </li>
+                 `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+
+function removeChannel(element) {
+    if (userEmotes.length > 1 && channelEmotes.length > 1) {
+        element.parentNode.parentNode.removeChild(element.parentNode);
+        userEmotes = userEmotes.filter(item => item != `https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${tempList[element.id].channelId}`);
+        channelEmotes = channelEmotes.filter(item => item != `https://api.betterttv.net/3/cached/users/twitch/${tempList[element.id].channelId}`);
+        tempList[element.id].trash = true;
+
+        localStorage.setItem("UElist",  JSON.stringify(userEmotes))
+        localStorage.setItem("CElist",  JSON.stringify(channelEmotes))
+        localStorage.setItem("Tlist",  JSON.stringify(tempList))
+    }
+    else {
+        alert("There must be at least one channel in list")
+    }
+}
+
+list.addEventListener("click", (event) => {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob == "delete") {
+        removeChannel(element);
+    }
+});
+
+
+document.addEventListener("keyup", (event) => {
+    if (event.key === 'Enter') {
+        const elem = input.value;
+        if (elem) {
+            Channel.getChannelId(elem)
+                .then(result => {
+                    if (result != "No such streamer :C") {
+                        addChannelName(elem, tempId, false);
+                        userEmotes.push(`https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${result}`);
+                        channelEmotes.push(`https://api.betterttv.net/3/cached/users/twitch/${result}`);
+
+                        localStorage.setItem("UElist",  JSON.stringify(userEmotes))
+                        localStorage.setItem("CElist",  JSON.stringify(channelEmotes))
+
+                        tempList.push({
+                            id: tempId,
+                            name: elem,
+                            channelId: result,
+                            trash: false
+                        });
+                        localStorage.setItem("Tlist",  JSON.stringify(tempList))
+                        console.log(userEmotes);
+                        console.log(channelEmotes);
+                        tempId++;
+                        document.location.reload();
+                    }
+                });
+            
+        }
+        input.value = '';
+    }
+});
+
 
 (function(userEmotes, channelEmotes) {
     const _privateLog = console.log;
